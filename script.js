cf10e81a7c135de3390b57df4efa6709ee011593
vinigrande2 <vinigrande2@gmail.com>
2026-04-07 22:35:34 -0300

    Add click event to button for alert message

---
+++ script.js
@@ -0,0 +1,4 @@
+// Seleciona o botão pelo ID e adiciona o evento de clique
+document.getElementById('meuBotao').addEventListener('click', function() {
+    alert('Olá! Obrigado por visitar meu portfólio web.');
+});
